b97fde5784d5062180e3f57fc861b33f2e0faeba
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-24 06:35:03 +0300

    20221124063503

--- 20221124/pic.php
+++ 20221124/pic.php
@@ -1,4 +1,6 @@
 <?php
-$im = imagecreate(800,600);
+$im = imagecreatetruecolor(800,600);
 header("Content-Type: image/jpeg");
+$c1 = imagecolorallocatealpha($im, 255, 0, 0, 64);
+imagefilledrectangle($im,100,100,200,300,$c1);
 imagejpeg($im);
