2e150899671d04cc76d0a0c682183302167ca0ff
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-24 06:40:03 +0300

    20221124064003

--- 20221124/pic.php
+++ 20221124/pic.php
@@ -1,6 +1,17 @@
 <?php
 $im = imagecreatetruecolor(800,600);
 header("Content-Type: image/jpeg");
-$c1 = imagecolorallocatealpha($im, 255, 0, 0, 64);
-imagefilledrectangle($im,100,100,200,300,$c1);
+$n = $_GET["n"] ?? 1;
+for ($i = 0; $i < $n; $i++) {
+  $c1 = imagecolorallocatealpha($im, 
+    rand(0,255), rand(0,255), rand(0,255), 64);
+  imagefilledrectangle($im,rand(0,800),rand(0,600),
+    rand(0,800),rand(0,600),$c1);
+}
+
+$c1 = imagecolorallocatealpha($im, 
+    255, 255, 255, 64);
+imagestring($im, 3, 200,200,"Hello, n=$n", imagecolorallocatealpha($im, 
+    255, 255, 255, 64));
+
 imagejpeg($im);
