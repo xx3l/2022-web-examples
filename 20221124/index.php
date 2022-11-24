a7d3a8a4c58ce1bb41b3ba95411bb32a75e71b08
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-24 06:00:04 +0300

    20221124060004

--- 20221124/index.php
+++ 20221124/index.php
@@ -1,7 +1,9 @@
 <?php
-$questions = [1,2,4,5,7,8];
+$questions = [1,2,4,5,7,8,9,10,11];
 $a = [];
 for($i = 0; $i < 5; $i++) {
 	$n = rand(0, count($questions)-1);
-	print "$n ";
+	$a[] = $questions[$n];
+	array_splice($questions, $n, 1);
 }
+print json_encode($a);
