fd6c4706bda2de924ff64f25f465872f1b2cbd19
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-25 10:05:03 +0300

    20221125100503

--- 20221125/pic.php
+++ 20221125/pic.php
@@ -1,4 +1,8 @@
 <?php
+$n = $_GET["n"] ?? 0;
+$m = $_GET["m"] ?? 0;
 $im = imagecreatetruecolor(400,300);
+$c = imagecolorallocatealpha($im, 255, 16, 64, 64);
+imagefilledrectangle($im, $n, $n, $m, $m, $c);
 header("Content-Type: image/png");
 imagepng($im);
