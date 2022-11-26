381622fe0fdd2d7c4c5d56f2a4d6fa3ee803bbbc
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-26 09:30:03 +0300

    20221126093003

--- 20221126/index.php
+++ 20221126/index.php
@@ -1,4 +1,23 @@
 <?php
 
+$answers = [
+  [1, "ответ 1", true],
+  [3, "ответ 2", false],
+  [4, "ответ 3", false],
+  [5, "ответ 4", true],
+  [6, "ответ 5", false],
+  [8, "ответ 6", false],
+];
 
-print rand(4,6);
+
+$correct = [];
+$incorrect = [];
+foreach($answers as $answer) {
+	if ($answer[2] == true) {
+		$correct[] = $answer[0];
+	} else {
+		$incorrect[] = $answer[0];
+	}
+}
+print_r($correct);
+print_r($incorrect);
