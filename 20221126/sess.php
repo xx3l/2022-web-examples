87ea17a62ebfd239f4fb7beb3436e7b5d8d03419
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-26 10:00:04 +0300

    20221126100004

--- 20221126/sess.php
+++ 20221126/sess.php
@@ -3,13 +3,19 @@ session_start();
 $action = $_REQUEST["action"] ?? "";
 if ($action == "start") {
 	$_SESSION["test_started_at"]=date("Y-m-d H:i:s");
+	$_SESSION["questions_count"] = rand(3,7);
+	$_SESSION["current_question"] = 0;
 	print "Вы поехали!";
 }
-print ($_SESSION["test_started_at"] ?? "");
-
+print_r($_SESSION);
 ?>
 <form method="post">
   <input type="hidden" name="action" value="start">
   <input type="submit" value="Поехали">
 </form>
 <a href="?id=5">Типа какая-то навигация</a>
+<form method="post">
+  <input type="hidden" name="action" value="next">
+  <input type="submit" value="Следующий вопрос">
+</form>
+
