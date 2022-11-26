6e89fd320f243d2e9a31105d43720e71a2264cc3
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-26 10:25:03 +0300

    20221126102503

--- 20221126/pix.php
+++ 20221126/pix.php
@@ -5,7 +5,10 @@ $c2 = imagecolorallocate($im, 0, 0, 0);
 imagefilledrectangle($im, 5,5,995,295, $c);
 imagefilledrectangle($im, 6,6,994,294, $c2);
 for($i=0; $i<1000; $i++) {
-  imagesetpixel($im, $i, sin($i/10)*100+150, $c);
+//  imagesetpixel($im, $i, sin($i/10)*100+150, $c);
+  imagefilledrectangle($im, $i, sin($i/10)*100+150, 
+    $i+3, sin($i/10)*100+150+3, $c);
 }
+imagestring($im, 5, 20,20,"hello, ".date("H:i:s"),$c);
 header("Content-Type: image/png");
 imagepng($im);
