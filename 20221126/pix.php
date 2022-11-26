9a128512a6fbc5f55416ffca7cbbc93e5581ce86
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-26 10:20:03 +0300

    20221126102003

--- 20221126/pix.php
+++ 20221126/pix.php
@@ -1,4 +1,11 @@
 <?php
 $im = imagecreatetruecolor(1000,300);
-header("Content-Type: image/jpeg");
-imagejpeg($im);
+$c = imagecolorallocatealpha($im, 0, 255, 0, 64);
+$c2 = imagecolorallocate($im, 0, 0, 0);
+imagefilledrectangle($im, 5,5,995,295, $c);
+imagefilledrectangle($im, 6,6,994,294, $c2);
+for($i=0; $i<1000; $i++) {
+  imagesetpixel($im, $i, sin($i/10)*100+150, $c);
+}
+header("Content-Type: image/png");
+imagepng($im);
