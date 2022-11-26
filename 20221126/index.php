a477ac77a1197629a9a078d37c0ebe8230334b57
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-26 09:35:03 +0300

    20221126093503

--- 20221126/index.php
+++ 20221126/index.php
@@ -3,13 +3,11 @@
 $answers = [
   [1, "ответ 1", true],
   [3, "ответ 2", false],
-  [4, "ответ 3", false],
+  [4, "ответ 3", true],
   [5, "ответ 4", true],
   [6, "ответ 5", false],
   [8, "ответ 6", false],
 ];
-
-
 $correct = [];
 $incorrect = [];
 foreach($answers as $answer) {
@@ -21,3 +19,20 @@ foreach($answers as $answer) {
 }
 print_r($correct);
 print_r($incorrect);
+$pool = [];
+$n = rand(0, count($correct)-1);
+$pool[] = $correct[$n];
+array_splice($correct, $n, 1);
+print "-----------\n";
+print_r($pool);
+print_r($correct);
+print "-----------\n";
+$a = array_merge($correct, $incorrect);
+
+for($i = 0; $i < 3; $i++) {
+	$n = rand(0, count($a)-1);
+	$pool[] = $a[$n];
+	array_splice($a, $n, 1);
+}
+
+print_r($pool);
