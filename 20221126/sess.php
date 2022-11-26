d5989f0970498984aa5392ab899e492124ce14fb
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-26 09:55:03 +0300

    20221126095503

--- 20221126/sess.php
+++ 20221126/sess.php
@@ -1,8 +1,12 @@
 <?php
+session_start();
 $action = $_REQUEST["action"] ?? "";
 if ($action == "start") {
+	$_SESSION["test_started_at"]=date("Y-m-d H:i:s");
 	print "Вы поехали!";
 }
+print ($_SESSION["test_started_at"] ?? "");
+
 ?>
 <form method="post">
   <input type="hidden" name="action" value="start">
