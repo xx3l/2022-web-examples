3b1a75bb07e448bd138313b38a714c00c0edbb7a
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-29 06:10:04 +0300

    20221129061004

--- 202211/29/index.php
+++ 202211/29/index.php
@@ -3,7 +3,10 @@ session_start();
 print_r($_REQUEST);
 // SELECT id FROM quiz_questions
 $question_ids = [1,2,3,4,7,8,9,10,12];
-
+$action = $_REQUEST["action"] ?? "";
+if ($action == "start") {
+	$_SESSION["is_quiz_started"] = true;
+}
 $is_quiz_started = $_SESSION["is_quiz_started"] ?? false;
 
 if (!$is_quiz_started) {
