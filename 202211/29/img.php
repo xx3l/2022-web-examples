de36f449ce6f46e2107b707087f50d2929432faf
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-29 06:40:04 +0300

    20221129064004

--- 202211/29/img.php
+++ 202211/29/img.php
@@ -1,11 +1,12 @@
 <?php
 $im = imagecreatetruecolor(500, 300);
-header("Content-Type: image/jpeg");
+//header("Content-Type: image/gif");
 for($i=0; $i<100; $i++) {
   $col = imagecolorallocatealpha($im,
      rand(0,255), rand(0,255), rand(0,255), 
 	 64);
-  imageline($im, rand(0,500),rand(0,300),
+  imagefilledrectangle($im, rand(0,500),rand(0,300),
      rand(0,500),rand(0,300), $col);
 }
-imagejpeg($im);
+
+//imagepng($im);
