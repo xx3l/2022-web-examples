f30dac3e9aa9b971c17b141f6f0542519823ccc3
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-29 06:00:03 +0300

    20221129060003

--- 202211/29/index.php
+++ 202211/29/index.php
@@ -1,7 +1,20 @@
 <?php
+session_start();
+print_r($_REQUEST);
 // SELECT id FROM quiz_questions
 $question_ids = [1,2,3,4,7,8,9,10,12];
+
+$is_quiz_started = $_SESSION["is_quiz_started"] ?? false;
+
+if (!$is_quiz_started) {
 ?>
 <form method="post">
+<input type="hidden" name="action" value="start">
 <input type="submit" value="START_QUIZ">
 </form>
+<?php
+} else {
+?>
+Quiz is started
+<?php
+}
