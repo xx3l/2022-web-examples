ddb875ed04d4a99f6ed37d4e6144a2e50bc86ffd
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-11-30 06:05:03 +0300

    20221130060503

--- 202211/24/pic.php
+++ 202211/24/pic.php
@@ -3,19 +3,22 @@ $im = imagecreatetruecolor(800,600);
 //header("Content-Type: image/png");
 $n = $_GET["n"] ?? 1;
 
-$a = (object)["a" => 5, "b"=>[1,2]];
+//$a = (object)["a" => 5, "b"=>[1,2]];
 
-print $a->a;
+//print $a->a;
+
+var_dump((strpos("hello","h") == false));
 
 die();
 
-function random_rect(object $im, int $x, int $y) : void {
+function random_rect(object $im, int $x, int $y) : string|int {
 	print "draw rect";
 	$c1 = imagecolorallocatealpha($im, 
 		rand(0,255), rand(0,255), rand(0,255), 64);
 	imagefilledrectangle($im,
 		rand(0,$x),rand(0,$y),
 		rand(0,$x),rand(0,$y),$c1);
+	return [];
 }
 
 for ($i = 0; $i < $n; $i++) 
