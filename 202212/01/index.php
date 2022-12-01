c87d0ff050e7ca62037655bad6424a2f800eafbf
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-12-01 04:25:03 +0300

    20221201042503

--- 202212/01/index.php
+++ 202212/01/index.php
@@ -1,14 +1,9 @@
 <?php
 print_r($_REQUEST);
 $fn = $_REQUEST["fn"] ?? "";
-
-$command = ';';
-$x = 0.5;
-//eval ($command);
-//print $y;
 ?>
 <form method="post">
-<input type="text" name="fn" value="$y = sin($x)">
+<input type="text" name="fn" value="<?=$fn?>">
 <input type="submit" value="Draw a graph">
 </form>
 <?php
