26acea84e92692f026e9569ab94ed5de16afec22
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-12-01 03:55:03 +0300

    20221201035503

--- 202212/01/index.php
+++ 202212/01/index.php
@@ -1,4 +1,10 @@
 <?php
-$command = 'phpinfo();';
-eval ($command);
-print $x;
+$command = ';';
+$x = 0.5;
+//eval ($command);
+//print $y;
+?>
+<form method="post">
+<input type="text" name="fn" value="$y = sin($x)">
+<input type="submit" value="Draw a graph">
+</form>
