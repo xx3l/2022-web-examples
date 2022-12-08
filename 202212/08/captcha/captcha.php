cb0480102538b58325720401bf954e4c6b6aa4ef
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-12-08 06:10:03 +0300

    20221208061003

--- 202212/08/captcha/captcha.php
+++ 202212/08/captcha/captcha.php
@@ -1,3 +1,9 @@
 <?php
 session_start();
-print_r($_SESSION);
+$string = $_SESSION["captcha"];
+$im = imagecreatetruecolor(300,50);
+$color = imagecolorallocatealpha($im, 255, 255, 255, 0);
+imagestring($im, 5, 0, 0, $string, $color);
+imagettftext($im, 18, 3, 0, 0, $color, "CENTAUR.TTF", $string);
+//header("Content-Type: image/png");
+imagepng($im);
