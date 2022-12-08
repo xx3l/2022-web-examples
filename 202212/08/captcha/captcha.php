dc26eb5ef33926907b522478e7d86bbc2370c81d
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-12-08 06:15:04 +0300

    20221208061504

--- 202212/08/captcha/captcha.php
+++ 202212/08/captcha/captcha.php
@@ -2,8 +2,9 @@
 session_start();
 $string = $_SESSION["captcha"];
 $im = imagecreatetruecolor(300,50);
-$color = imagecolorallocatealpha($im, 255, 255, 255, 0);
-imagestring($im, 5, 0, 0, $string, $color);
-imagettftext($im, 18, 3, 0, 0, $color, "CENTAUR.TTF", $string);
-//header("Content-Type: image/png");
+$white = imagecolorallocatealpha($im, 128, 128, 128, 0);
+$black = imagecolorallocatealpha($im, 255, 255, 0, 0);
+imagettftext($im, 30, 3, 40, 40, $white, "./CENTAUR.TTF", $string);
+imagestring($im, 5, 100, 15, $string, $black);
+header("Content-Type: image/png");
 imagepng($im);
