fb48fcb52e045a2adacf59e5601500c350e06962
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-12-08 06:25:09 +0300

    20221208062509

--- 202212/08/captcha/index.php
+++ 202212/08/captcha/index.php
@@ -2,15 +2,16 @@
 session_start();
 if (isset($_POST["user"])) {
 	$captcha_answer = $_POST["captcha"] ?? "ну не знаю";
-	$correct_captcha_answer = $_SESSION["captcha"] ?? ""
-	if ($captcha_answer == ) {
+	$correct_captcha_answer = $_SESSION["captcha"] ?? "";
+	if ($captcha_answer == $correct_captcha_answer) {
 		print "Пошли проверять имя пользователя и пароль";
 	} else {
 		print "Капча не разгадана";
 	}
 } else {
 	print "Пока ничего пользователь не отправил";
-	$_SESSION["captcha"] = rand(100000000,999999999);
+//	$_SESSION["captcha"] = rand(100000000,999999999);
+	$_SESSION["captcha"] = rand(10,99);
 }
 ?>
 
