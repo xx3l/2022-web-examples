93d1f63d5075ebcea84a7de62a3031e58ef589de
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-12-09 10:00:04 +0300

    20221209100004

--- 202212/09/captcha.php
+++ 202212/09/captcha.php
@@ -1,4 +1,8 @@
 <?php
 session_start();
 $string = $_SESSION["captcha"] ?? "беда..";
-print_r($_SESSION);
+$im = imagecreatetruecolor(300,50);
+$white = imagecolorallocatealpha($im,255,255,255,.5);
+imagettftext($im, 24, 5, 30, 40, $white, "./comic.ttf", $string);
+header("Content-type: image/png");
+imagepng($im);
