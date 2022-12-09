8e83568ac50752b5afebb922b8355b3beb58d05f
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-12-09 09:30:03 +0300

    20221209093003

--- 202212/09/index.php
+++ 202212/09/index.php
@@ -1,1 +1,20 @@
-sdf
+<?php
+session_start();
+print_r($_POST);
+?>
+<form method="post">
+  <div>
+    Введите старый пароль
+    <input type="password" name="old_pw">
+  </div>
+  <div>
+    Введите новый пароль
+    <input type="password" name="new_p1">
+  </div>
+  <div>
+    <img src="captcha.php"><br/>
+    <input type="text" name="captcha">
+  </div>
+  
+  <input type="submit" value="сменить пароль">
+</form>
