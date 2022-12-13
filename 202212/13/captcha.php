7936d71c589e108dbdb18d961737660b2ba705e7
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2022-12-13 06:05:04 +0300

    20221213060504

--- 202212/13/captcha.php
+++ 202212/13/captcha.php
@@ -1,4 +1,7 @@
 <?php
 $im=imagecreatetruecolor(100,30);
+$yellow = imagecolorallocate($im, 255, 255, 0);
+$captcha = rand(100,999);
+imagestring($im, 5, 10, 10, $captcha, $yellow);
 header("Content-Type: image/png");
 imagepng($im);
