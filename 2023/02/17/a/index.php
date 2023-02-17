80cac6ec37fcc4967cc28913aef448005fddb84a
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-02-17 10:50:04 +0300

    20230217105004

---
+++ 2023/02/17/a/index.php
@@ -0,0 +1,13 @@
+<style>
+.common { background: #aea; }
+.div1 { background: #cea; width:40%;}
+.div2 { background: #ead; width:40%;}
+</style>
+<div class="common">
+	<div class="div1">
+	Это первый div
+	</div>
+	<div class="div2">
+	Это второй div
+	</div>
+</div>
