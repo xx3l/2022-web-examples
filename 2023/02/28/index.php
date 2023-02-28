e4b9d21e4b109a45a025e08868687161c07b79cf
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-02-28 06:05:03 +0300

    20230228060502

--- 2023/02/28/index.php
+++ 2023/02/28/index.php
@@ -4,24 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css1" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
   <div class="container">
-	  <nav aria-label="breadcrumb">
-	  <ol class="breadcrumb">
-		<li class="breadcrumb-item"><a href="#">Home</a></li>
-		<li class="breadcrumb-item"><a href="#">Library</a></li>
-		<li class="breadcrumb-item"><a href="#">Home</a></li>
-	  </ol>
-	  </nav>
-	  
-		<div class="btn-group" aria-label="Vertical radio toggle button group">
-		  <button type="button" class="btn btn-danger">Left</button>
-		  <button type="button" class="btn btn-warning">Middle</button>
-		  <button type="button" class="btn btn-success">Right</button>
-		</div>
+	<select name="some_type">
+	  <option>Type 1</option>
+	  <option>Type 2</option>
+	  <option>kjnf;kjasndf;kjnsad fjkn;aksjdnf k;jasdn f;kjasdn f;kjasdn f</option>
+	</select>
   </div>
   </body>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js1" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </html>
