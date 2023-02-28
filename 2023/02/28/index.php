e3a94125225df6803050f2803456cd61d5cc9005
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-02-28 05:45:03 +0300

    20230228054503

--- 2023/02/28/index.php
+++ 2023/02/28/index.php
@@ -16,7 +16,11 @@
 	  </ol>
 	  </nav>
 	  
-	  <button class="btn btn-info">Primary</button>
+		<div class="btn-group" aria-label="Vertical">
+		  <button type="button" class="btn btn-danger">Left</button>
+		  <button type="button" class="btn btn-warning">Middle</button>
+		  <button type="button" class="btn btn-success">Right</button>
+		</div>
   </div>
   </body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
