41d65ea64ea2f76bf0caa59b1bfb7795eb377b32
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-03-07 05:10:04 +0300

    20230307051004

---
+++ 2023/03/07/index.php
@@ -0,0 +1,14 @@
+<div class="row">
+  <div class="cell"></div>
+  <div class="cell"></div>
+  <div class="cell"></div>
+</div>
+<div class="row">
+  <div class="cell"></div>
+  <div class="cell"></div>
+  <div class="cell"></div>
+</div><div class="row">
+  <div class="cell"></div>
+  <div class="cell"></div>
+  <div class="cell"></div>
+</div>
