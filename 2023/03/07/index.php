73364cde76e1b016fc2fbcc753fd3992aaa8f5b5
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-03-07 05:15:03 +0300

    20230307051503

--- 2023/03/07/index.php
+++ 2023/03/07/index.php
@@ -1,3 +1,14 @@
+<style>
+.row {}
+.cell {
+	display: inline-block;
+	width: 50px;
+	height: 50px;
+	border: 1px black solid;
+	margin: 3px;
+	background: #eea;
+}
+</style>
 <div class="row">
   <div class="cell"></div>
   <div class="cell"></div>
@@ -12,3 +23,6 @@
   <div class="cell"></div>
   <div class="cell"></div>
 </div>
+<template id="figure">
+12312
+</template>
