4d25f4291d56459eb572ffba9d436c8082c6149d
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-03-10 09:45:03 +0300

    20230310094503

--- 2023/03/10/index.php
+++ 2023/03/10/index.php
@@ -1,1 +1,10 @@
-<button>Make HTTP GET request to server</button>
+<input id="param"><input id="value">
+<button id="send">Make HTTP GET request to server</button>
+<script>
+window.onload = () => {
+	console.log("loaded");
+	document.getElementById("send").onclick = () => {
+		console.log("clicked");
+	}
+}
+</script>
