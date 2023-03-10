262915842f3a8f8ef5ba69b4d59716eb736b4531
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-03-10 10:00:03 +0300

    20230310100003

--- 2023/03/10/index.php
+++ 2023/03/10/index.php
@@ -1,12 +1,19 @@
 <input id="param" value="var"><input id="value" value="5">
 <button id="send">Make HTTP GET request to server</button>
+<div id="response"></div>
 <script>
 window.onload = () => {
 	console.log("loaded");
 	document.getElementById("send").onclick = () => {
-		params = "?" + document.getElementById("param").value + "="
+		params = "backend.php?" + document.getElementById("param").value + "="
 			+ document.getElementById("value").value
 		console.log(params);
+		fetch(params)
+			.then(response => response.text())
+			.then(data => {
+				document.getElementById("response").innerText = data;
+			});
+		console.log("code is woking still");
 	}
 }
 </script>
