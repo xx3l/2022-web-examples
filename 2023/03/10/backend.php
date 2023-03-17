435d720719eec261c0a181bba7a6c9b8d7b6b9c3
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-03-17 09:30:04 +0300

    20230317093004

--- 2023/03/10/backend.php
+++ 2023/03/10/backend.php
@@ -4,6 +4,17 @@ for ($i = 0; $i < 5; $i++) {
 	$a[$i] = [];
 	$a[$i]["numer1"] = rand(1, 20);
 	$a[$i]["numer2"] = rand(1, 20);
+	$action_array = ["+", "-", "/", "*"];
+	$rand_action = $action_array[rand(0, count($action_array))];
+	$a[$i]["action"] = $rand_action;
+	if ($rand_action == "+")
+		$a[$i]["answer"] = $a[$i]["numer1"] + $a[$i]["numer2"];
+	if ($rand_action == "-")
+		$a[$i]["answer"] = $a[$i]["numer1"] - $a[$i]["numer2"];
+	if ($rand_action == "*")
+		$a[$i]["answer"] = $a[$i]["numer1"] * $a[$i]["numer2"];
+	if ($rand_action == "/")
+		$a[$i]["answer"] = $a[$i]["numer1"] / $a[$i]["numer2"];
 	
 }
 print json_encode($a);
