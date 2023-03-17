56e3539b250b9ccfdee0738b1b5e84503c25d874
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-03-17 09:15:04 +0300

    20230317091504

--- 2023/03/10/backend.php
+++ 2023/03/10/backend.php
@@ -1,11 +1,3 @@
 <?php
-$v1 = $_REQUEST['v1'] ?? 0;
-$v2 = $_REQUEST['v2'] ?? 0;
-$a = [
-	"v1" => $v1,
-	"v2" => $v2,
-	"sum" => $v1 + $v2,
-	"sub" => $v1 - $v2,
-	"mul" => $v1 * $v2,
-];
+$a = [];
 print json_encode($a);
--- 2023/03/10/index.php
+++ 2023/03/10/index.php
@@ -7,8 +7,14 @@
 <button>Give me more!</button>
 </body>
 <script>
+function getSomeMore() {
+	fetch("backend.php")
+		.then(resp => resp.json())
+		.then(json => console.log(json))
+}
 window.onload = function() {
 	console.log("page is loaded");
+	getSomeMore();
 }
 </script>
 </html>
