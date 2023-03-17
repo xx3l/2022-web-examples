95d48d9d2d06a9e94514b9de4ca7c19ba2883f78
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-03-17 09:35:03 +0300

    20230317093503

--- 2023/03/10/backend.php
+++ 2023/03/10/backend.php
@@ -5,7 +5,7 @@ for ($i = 0; $i < 5; $i++) {
 	$a[$i]["numer1"] = rand(1, 20);
 	$a[$i]["numer2"] = rand(1, 20);
 	$action_array = ["+", "-", "/", "*"];
-	$rand_action = $action_array[rand(0, count($action_array))];
+	$rand_action = $action_array[rand(0, count($action_array)-1)];
 	$a[$i]["action"] = $rand_action;
 	if ($rand_action == "+")
 		$a[$i]["answer"] = $a[$i]["numer1"] + $a[$i]["numer2"];
@@ -13,8 +13,14 @@ for ($i = 0; $i < 5; $i++) {
 		$a[$i]["answer"] = $a[$i]["numer1"] - $a[$i]["numer2"];
 	if ($rand_action == "*")
 		$a[$i]["answer"] = $a[$i]["numer1"] * $a[$i]["numer2"];
-	if ($rand_action == "/")
+	if ($rand_action == "/") {
+		$b = rand(1, 10);
+		$c = rand(1, 10);
+		$d = $b * $c;
+		$a[$i]["numer1"] = $d;
+		$a[$i]["numer2"] = $b;
 		$a[$i]["answer"] = $a[$i]["numer1"] / $a[$i]["numer2"];
+	}
 	
 }
 print json_encode($a);
