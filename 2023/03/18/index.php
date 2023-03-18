bb52a2010f535a80ea667a89fbc4c33fa0499cee
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2023-03-18 07:35:03 +0300

    20230318073503

--- 2023/03/18/index.php
+++ 2023/03/18/index.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
 	<link rel="stylesheet" href="bootstrap.css">
+    <script src="bootstrap.js"></script>
+
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+    <h1 class="p-5">Hello, world!</h1>
   </body>
 </html>
